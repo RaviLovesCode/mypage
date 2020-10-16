a427b10702b388d96276095fd6a2f2320907f4c1
RaviLovesCode <72981534+RaviLovesCode@users.noreply.github.com>
2020-10-16 21:05:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,36 +2,80 @@ let canvas = document.querySelector('canvas')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-let c = canvas.getContext('2d')
-let x = 100
-let y = 100
-let radius = 50
-let dx = 5
-let dy = 5
+// Circle(x, y, radius, dx, dy){
+//    this.x = x
+//    this.y = y
+//    this.dx = dx
+//    this.dy = dy
+//    this.radius = radius
+//    function draw(){
 
- function draw(){
-	c.beginPath()
-	c.fillStyle = 'lime'
-	c.arc(x, y, radius, 0, Math.PI * 2, false)
-	c.closePath()
-}
-function update(){
-	x += dx
-	y += dy
-	if(x  + radius > innerWidth || x - radius < 0){
-		dx = -dx
-	}
-	if(y  + radius > innerHeight || y - radius < 0){
-		dy = -dy
-	}
-}
+//    }
+//    function update(){
+   	
+//    }
+// }
+
+
+ let c = canvas.getContext('2d')
+ let x = 100
+ let y = 100
+ let radius = 50
+ let dx = 5
+ let dy = 5
 
 function move(){
-	requestAnimationFrame(move)
+    requestAnimationFrame(move)
 	c.clearRect(0, 0, innerWidth, innerHeight)
-    draw()
-    update()
-
-
-}
+    c.beginPath()
+    c.fillStyle = " lightGreen"
+    c.arc(x, y, radius, 0, Math.PI * 2, false)
+    c.fill()
+    c.closePath()
+    x += dx
+    y += dy
+    if(x + radius > innerWidth || x - radius< 0 ){
+        dx = -dx
+    }
+    if(y + radius > innerHeight || y - radius< 0 ){
+        dy = -dy
+    }
+   
+ }
 move()
+
+
+
+// let x = 100
+// let y = 100
+// let radius = 50
+// let dx = 5
+// let dy = 5
+// console.log(c)
+
+//  function draw(){
+// 	c.beginPath()
+// 	c.fillStyle = 'lime'
+// 	c.arc(x, y, radius, 0, Math.PI * 2, false)
+// 	c.closePath()
+// }
+// function update(){
+// 	x += dx
+// 	y += dy
+// 	if(x  + radius > innerWidth || x - radius < 0){
+// 		dx = -dx
+// 	}
+// 	if(y  + radius > innerHeight || y - radius < 0){
+// 		dy = -dy
+// 	}
+// }
+
+// function move(){
+// 	requestAnimationFrame(move)
+// 	c.clearRect(0, 0, innerWidth, innerHeight)
+//     draw()
+//     update()
+
+
+// }
+// move()
