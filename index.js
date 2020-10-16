7cfd55a94bb3e6a01c6981373d3f9f0b148ab143
RaviLovesCode <72981534+RaviLovesCode@users.noreply.github.com>
2020-10-16 21:26:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let canvas = document.querySelector('canvas')
-canvas.width = innerWidth
-canvas.height = innerHeight
+let canvas = document.querySelector('canvas');
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 // Circle(x, y, radius, dx, dy){
 //    this.x = x
@@ -17,28 +17,28 @@ canvas.height = innerHeight
 // }
 
 
- let c = canvas.getContext('2d')
- let x = 100
- let y = 100
- let radius = 50
- let dx = 5
- let dy = 5
+ let c = canvas.getContext('2d');
+ let x = 100;
+ let y = 100;
+ let radius = 50;
+ let dx = 5;
+ let dy = 5;
 
 function move(){
-    requestAnimationFrame(move)
-	c.clearRect(0, 0, innerWidth, innerHeight)
-    c.beginPath()
-    c.fillStyle = " lightGreen"
-    c.arc(x, y, radius, 0, Math.PI * 2, false)
-    c.fill()
-    c.closePath()
-    x += dx
-    y += dy
+    requestAnimationFrame(move);
+	c.clearRect(0, 0, innerWidth, innerHeight);
+    c.beginPath();
+    c.fillStyle = " lightGreen";
+    c.arc(x, y, radius, 0, Math.PI * 2, false);
+    c.fill();
+    c.closePath();
+    x += dx;
+    y += dy;
     if(x + radius > innerWidth || x - radius< 0 ){
-        dx = -dx
+        dx = -dx;
     }
     if(y + radius > innerHeight || y - radius< 0 ){
-        dy = -dy
+        dy = -dy;
     }
    
  }
